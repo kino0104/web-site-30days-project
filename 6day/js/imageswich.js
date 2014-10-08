729412661e14e2fbb9b7cc678e9d1b896857980a
kyousuke kinoshita <kyousuke.kinoshita@gmail.com>
2014-10-08 19:56:11 +0900

    change css 6day

---
+++ 6day/js/imageswich.js
@@ -0,0 +1,11 @@
+onload = function(){
+var attrs = [],
+	photo = document.getElementById('photo'),
+	thumbnail = document.getElementsByClassName('thumbnail');
+
+ 	 for(var i = 0; i < thumbnail.length; i++){
+ 	 	thumbnail[i].onclick = function(){
+ 	 	photo.setAttribute('src', this.getAttribute('src'));
+ 	 	};
+ 	 }
+};
