85d68bca5d5b939bbfc928b25a46ba25265ed103
kyousuke kinoshita <kyousuke.kinoshita@gmail.com>
2014-10-11 11:20:50 +0900

    add 8day

---
+++ 8day/js/box.js
@@ -0,0 +1,44 @@
+window.onload = function(){
+	var container = document.getElementById('container'),
+		queue = null,
+		wait = 300,
+		canvas = document.getElementById('box');
+	
+	canvasSize();
+	draw();
+	
+	function canvasSize(){
+		canvas.width = container.offsetWidth;
+		canvas.height = container.offsetHeight;
+	}
+	
+	window.addEventListener("resize",function(){
+		clearTimeout(queue);
+		queue = setTimeout(function(){
+			canvasSize();
+		}, wait);
+	},false);
+	
+	function draw(){
+		if(!canvas || !canvas.getContext) return false;
+
+		var ctx = canvas.getContext('2d');
+	
+		(function loop(){
+			ctx.clearRect(0,0,canvas.width,canvas.height);
+			var x=[],y=[],wh=[],r=[],g=[],b=[];
+		
+			for(var i=0; i<Math.floor(Math.random() * 500); i++){
+				x[i] = Math.floor(Math.random() * (canvas.width - 50)),
+				y[i] = Math.floor(Math.random() * (canvas.height -50)),
+				wh[i] = Math.floor(Math.random() * 50),
+				r[i] = Math.floor(Math.random() * 255),
+				g[i] = Math.floor(Math.random() * 255),
+				b[i] = Math.floor(Math.random() * 255);
+				ctx.fillStyle = "rgb("+r[i]+","+g[i]+","+b[i]+")";
+				ctx.fillRect(x[i],y[i],wh[i],wh[i]);
+			}
+			setTimeout(loop,1000);
+		})();
+	}
+}
